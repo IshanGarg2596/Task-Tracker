962449861bba17fba4168295b5c4e752759dc3e0
Ishan  Garg <ishan@Ishans-MacBook-Air.local>
2021-07-28 20:09:54 +0530

    changed react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
